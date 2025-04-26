59f42f968e6769b366ed7fcd01a4548095d2274c
Rodgie12 <rodgersndiege@gmail.com>
2025-04-26 15:17:39 +0300

    changed index.php

--- index.php
+++ index.php
@@ -5,14 +5,16 @@
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
-    <h1>Welcome to the Health Information System</h1>
+    <h1>Welcome to Health Information System</h1>
     <nav>
         <ul>
+
             <li><a href="views/create_program.php">Create Health Program</a></li>
             <li><a href="views/register_client.php">Register New Client</a></li>
             <li><a href="views/enroll_client.php">Enroll Client in Program</a></li>
             <li><a href="views/search_client.php">Search for Client</a></li>
             <li><a href="views/view_clients.php">View Clients</a></li>
+
         </ul>
     </nav>
 </body>
